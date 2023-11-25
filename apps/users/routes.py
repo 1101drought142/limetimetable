07ba117a0616db6b7eca5f4b2aa724d31733fb4e
Konstantin Bagatelia <konstantinbagatelia@mail.ru>
2023-11-25 16:41:48 +0300

    new structure

--- apps/users/routes.py
+++ apps/users/routes.py
@@ -4,7 +4,7 @@
 #     return {"message": "Logged in successfully", "session_id": session_id}
 from typing import Annotated
 
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Request, Depends, status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
@@ -14,6 +14,13 @@ import apps.users.models as user_models
 from apps.users.logic import isadmin
 from database import get_db
 
+LOGOUT_HTML = """
+<center>
+    <h1>Войти</h1>
+    <a href="/"><button>Войти</button></a>
+</center>
+"""
+
 router = APIRouter(prefix='/user', tags=['users'])
 templates = Jinja2Templates(directory="templates")
 
@@ -21,3 +28,7 @@ templates = Jinja2Templates(directory="templates")
 def logs(request: Request, user: Annotated[user_models.User, Depends(isadmin)], db: Session = Depends(get_db)):
     logs = user_queries.get_logs(db)
     return templates.TemplateResponse("userlog.html", {"request": request, "logs" : logs})
+
+@router.post("/logout")
+async def logout():
+    return HTMLResponse(content=LOGOUT_HTML, status_code=status.HTTP_401_UNAUTHORIZED)
