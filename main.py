1517256925f9cb88214b88a558f55cdda7732259
Konstantin Bagatelia <konstantinbagatelia@mail.ru>
2023-12-03 15:02:35 +0300

    cors fix

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    max_age = 0,
 )
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
