b15fe9676d29e4f528d0a60963c1e5d4cebe97e1
Konstantin Bagatelia <konstantinbagatelia@mail.ru>
2023-12-06 16:57:07 +0300

    test raspisanie

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
+import logging
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
@@ -13,10 +14,15 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 class Base(DeclarativeBase): 
     pass
 
+logger = logging.getLogger(__name__)
 # Dependency
 def get_db():
     db = SessionLocal()
     try:
         yield db
+    except Exception:
+        logger.exception("Session rollback because of exception")
+        db.rollback()
+        raise
     finally:
         db.close()
