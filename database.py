e759b38f74a23dca0e108d1c7c653254e7d99dff
root <root@qfrgatsurh.local>
2023-12-10 15:28:40 +0300

    env files

--- database.py
+++ database.py
@@ -1,10 +1,17 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
 import logging
 
+load_dotenv(find_dotenv())
+print(find_dotenv())
+print(os.environ.get("BD_LOGIN"))
+
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:new-password@localhost/test"
+SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{os.environ.get("BD_LOGIN")}:{os.environ.get("BD_PASSWORD")}@{os.environ.get("BD_HOST")}/test'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
@@ -14,7 +21,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 class Base(DeclarativeBase): 
     pass
 
-logger = logging.getLogger(__name__)
+
 # Dependency
 def get_db():
     db = SessionLocal()
